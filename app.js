3c51cc151b562891092f345963b1bc8e15fe4e94
vivekrajshi <rajshiraj454@gmail.com>
2025-06-27 12:32:37 +0530

    fix: 404 error handler for render

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
-app.all("/", (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new ExpressError("Page not found", 404));
 });
 
